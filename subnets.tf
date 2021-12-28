dc30669b33844eb6586c808650d1e84eb36cc0fc
tintin <christinemoreno424@gmail.com>
2021-12-28 13:43:59 +0800

    Adding subnet module to my terraform files.

---
+++ subnets.tf
@@ -0,0 +1,12 @@
+module "subnets" {
+  source             = "clouddrove/terraform-aws-subnet/aws"
+  version            = "0.15.0"
+  name               = "subnets"
+  environment        = "dev"
+  label_order        = ["name", "environment"]
+  availability_zones = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  vpc_id             = module.vpc.vpc_id
+  type               = "public"
+  igw_id             = "igw-5ea15639"
+  cidr_block         = module.vpc.vpc_cidr_block
+}
--- vpc.tf
+++ vpc.tf
@@ -8,8 +8,11 @@ module "vpc" {
   source     = "terraform-aws-modules/vpc/aws"
   create_vpc = false
 
+  name = "dev-vpc"
+  cidr = "172.31.0.0/16"
+
   manage_default_vpc               = true
-  default_vpc_name                 = "default"
+  default_vpc_name                 = "default-vpc"
   default_vpc_enable_dns_hostnames = true
 
   tags = merge(var.default_tags)
