ec302e3df12da62537fb772fbb1cdd301530ccda
tintin <christinemoreno424@gmail.com>
2021-12-28 12:27:03 +0800

    Adding terraform files.

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 module "vpc" {
   source     = "terraform-aws-modules/vpc/aws"
   create_vpc = false
